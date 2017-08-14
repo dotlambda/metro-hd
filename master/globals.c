8772be9d372b4dd422f2ee04f414754e41898d51
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-08-14 20:10:06 +0200

    Add ceiling as obstacle

--- master/globals.c
+++ master/globals.c
@@ -3,6 +3,8 @@
 
 long obstacle(uint8_t x, uint8_t y)
 {
+    if (y == 5) // ceiling
+        return 1l;
     if (doors & 0b00000010 && (x < 4 || (y >= 20 && x < 6)))
         return 1l;
     else if (doors & 0b00000001 && (x >= DISPLAY_WIDTH - 4 || (y >= 20 && x >= DISPLAY_WIDTH - 6)))
