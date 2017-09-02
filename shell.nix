37b24a39790bf9f1244de0f55002eda715ac9878
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-09-02 21:27:46 +0200

    Python script to convert MIDI into C arrays

--- shell.nix
+++ shell.nix
@@ -7,12 +7,22 @@ let
     sha256 = "0cacw30vy4xswpkj3vbw92xfv5q06mw22msq0i54gphmw2r5iizh";
   };
   pkgs     = import pinned {};
+  midi     = pkgs.python2Packages.buildPythonPackage rec {
+    name = "midi-${version}";
+    version = "0.2.3";
+    src = fetch {
+      owner  = "vishnubob";
+      repo   = "python-midi";
+      rev    = "4b7a229f6b340e7424c1fccafa9ac543b9b3d605";
+      sha256 = "1pldwrz3yfavjlc9n0frvdfyqnmn3rcgy701pc4424lmqp18c6zj";
+    };
+  };
 in pkgs.stdenv.mkDerivation {
   name = "metroid";
   buildInputs = with pkgs; [
     avrdude avrgcclibc
-    (python3.withPackages (ps: with ps; [numpy scikitimage]))
-    python2
+    (python3.withPackages (ps: with ps; [numpy scikitimage])) # for sprites
+    (python2.withPackages (ps: with ps; [midi]))              # for sound
   ];
 }
 
---
+++ slave/music.h
@@ -0,0 +1,11 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include <inttypes.h>
+
+#include <avr/pgmspace.h>extern PROGMEM const uint8_t title-screen_tones**;
+extern PROGMEM const uint8_t title-screen_events**;
+extern PROGMEM const uint16_t title-screen_delays**;
+
+
+#endif
