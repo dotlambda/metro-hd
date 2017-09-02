02effa30c5fe577aad26f16bbb5aedc22efb4c81
Console B <2017ss_console_b@preston.(none)>
2017-09-02 16:30:14 +0200

    Changed recharge label and drawing

--- master/drawing.c
+++ master/drawing.c
@@ -338,23 +338,17 @@ void drawsprite_px_inverted(uint8_t x, uint8_t y, uint8_t width, uint8_t height,
 // color is actually the byte that will be used for each page
 void drawcolor(uint8_t x, uint8_t y, uint8_t width, uint8_t height, uint8_t color)
 {
-    sendbyte(0b11110100, 0); // set window start column
-    sendbyte(x, 0);
-    sendbyte(0b11110110, 0); // set window end column
-    sendbyte(x + width - 1, 0);
-    sendbyte(0b11110101, 0); // set window start page
-    sendbyte(y, 0);
-    sendbyte(0b11110111, 0); // set window end page
-    sendbyte(y + height - 1, 0);
-    sendbyte(0b11111001, 0); // enable window function
+    enable_window(x, y, width, height)
     for (uint16_t i = 0; i < width * height; ++i)
         sendbyte(color, 1);
-    sendbyte(0b11111000, 0); // disable window function
+    disable_window();
 }
 
 void drawrechargeroom()
 {
-    drawsprite(4, 6, 152, 4, labelrecharge);
+    drawcolor(4, 6, 19, 4, 0xFF);
+    drawcolor(134, 6, 18, 4, 0xFF);
+    drawsprite(23, 6, 115, 4, labelrecharge);
     drawsprite(DISPLAY_WIDTH/2 - 12, 10, 24, 7, rechargetop);
     drawsprite(DISPLAY_WIDTH/2 - 12, 23, 24, 2, mountain); 
 }
