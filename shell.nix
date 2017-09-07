ef66cd1a6ffea22a4295b92c8b47091cc588b743
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-09-07 23:54:30 +0200

    Simple array generation based on existing synth.py

--- shell.nix
+++ shell.nix
@@ -1,20 +1,18 @@
 let 
-  fetch    = (import <nixpkgs> {}).fetchFromGitHub;
-  pinned   = fetch {
-    owner  = "NixOS";
-    repo   = "nixpkgs";
-    rev    = "56da88a298a6f549701a10bb12072804a1ebfbd5"; # NixOS 17.03
-    sha256 = "0cacw30vy4xswpkj3vbw92xfv5q06mw22msq0i54gphmw2r5iizh";
+  fetch      = (import <nixpkgs> {}).fetchFromGitHub;
+  pinned     = fetch {
+    owner    = "NixOS";
+    repo     = "nixpkgs";
+    rev      = "56da88a298a6f549701a10bb12072804a1ebfbd5"; # NixOS 17.03
+    sha256   = "0cacw30vy4xswpkj3vbw92xfv5q06mw22msq0i54gphmw2r5iizh";
   };
-  pkgs     = import pinned {};
-  midi     = pkgs.python2Packages.buildPythonPackage rec {
-    name = "midi-${version}";
-    version = "0.2.3";
-    src = fetch {
-      owner  = "vishnubob";
-      repo   = "python-midi";
-      rev    = "4b7a229f6b340e7424c1fccafa9ac543b9b3d605";
-      sha256 = "1pldwrz3yfavjlc9n0frvdfyqnmn3rcgy701pc4424lmqp18c6zj";
+  pkgs       = import pinned {};
+  mido       = pkgs.python2Packages.buildPythonPackage rec {
+    name     = "mido-${version}";
+    version  = "1.2.8";
+    src      = pkgs.fetchurl {
+      url    = "mirror://pypi/m/mido/mido-${version}.tar.gz";
+      sha256 = "0griab8f54d3phf07p99s750xjw7vx2sdhydxgyca7ajsis2h51m";
     };
   };
 in pkgs.stdenv.mkDerivation {
@@ -22,7 +20,7 @@ in pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     avrdude avrgcclibc
     (python3.withPackages (ps: with ps; [numpy scikitimage])) # for sprites
-    (python2.withPackages (ps: with ps; [midi]))              # for sound
+    (python2.withPackages (ps: with ps; [mido]))              # for sound
   ];
 }
 
--- slave/synth.py
+++ slave/synth.py
@@ -1,95 +1,96 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
-import serial, time
 from mido import MidiFile
 
 n_octave=6
 
-# $ pip install mido
+hfilename = "music.h"
+cfilename = "music.c"
 
 class Synth():
-	def __init__(self):
+    def __init__(self, noteOffset, hfile, cfile):
+        self.noteOffset = noteOffset
+        self.hfile = hfile
+        self.cfile = cfile
+    def appendTone(self,nr, onOff):
+        if nr<0 or nr>n_octave*12:
+            print "-----------",nr,"out of range"
+            return False
+        assert(nr < (1 << 7)) # the most significant bit is used for storing onOff
+        # append tone to array
+        array = ""
+        if onOff:
+            array += str(nr | (1 << 7))
+        else:
+            array += str(nr)
+        array += ", "
+        return array
+    def appendTime(self, time): # time is in ms
+        print time
+        assert(time < 2 ** 32) # times are stored as uint32_t
+        #TODO check relative error
+        # append time to array
+        array = str(int(round(time)))
+        array += ", "
+        return array
+    def writeCArray(self,filename,arrayname,includeTracks=[]):
+        mid = MidiFile(filename)
+        events=[]
+        print "Number of tracks:",len(mid.tracks)
+        for i, track in enumerate(mid.tracks):
+            print('Track {}: {}'.format(i, track.name))
+            if not (includeTracks==[] or i in includeTracks):
+                continue
+            t=0
+            for message in track:
+                t+=message.time
+                if (message.type=="note_on" or message.type=="note_off") :
+                    e={}
+                    if message.velocity==0:
+                        e["type"]="note_off"
+                    else:
+                        e["type"]=message.type
+                    e["note"]=message.note
+                    events.append((t,e))
+                elif message.type=="set_tempo":
+                    e={}
+                    e["type"]=message.type
+                    e["tempo"]=message.tempo
+                    events.append((t,e))
+        tempo=600000
+        events.sort(key=lambda tup: tup[0])
+        tones_array = "const PROGMEM uint8_t " + arrayname + "_tones[] = {\n    "
+        times_array = "const PROGMEM uint32_t " + arrayname + "_times[] = {\n    "
+        nr_note_events = 0
+        for e in events:
+            time = e[0]*10**(-3)/480.*tempo # in ms
+            ev=e[1]
+            if ev["type"]=="note_on":
+                print "PLAY  ",ev["note"]-self.noteOffset
+                new_tone = self.appendTone(ev["note"]-self.noteOffset,True)
+                if new_tone:
+                    tones_array += new_tone
+                    times_array += self.appendTime(time)
+                    nr_note_events += 1
+            elif ev["type"]=="note_off":
+                print "STOP ",ev["note"]-self.noteOffset
+                new_tone = self.appendTone(ev["note"]-self.noteOffset,False)
+                if new_tone:
+                    tones_array += new_tone
+                    times_array += self.appendTime(time)
+                    nr_note_events += 1
+            elif ev["type"]=="set_tempo":
+                tempo=ev["tempo"]
+                print "TEMPO ", tempo,"=",60000000./tempo,"BPM"
+        tones_array = tones_array[:-2] + "\n};"
+        times_array = times_array[:-2] + "\n};"
+        cfile.write(tones_array)
+        cfile.write("\n")
+        cfile.write(times_array)
+        cfile.write("\n\n")
 
-		self.ser = serial.Serial()
-		self.ser.port = "/dev/ttyS0"
-		self.ser.baudrate = 9600
-		self.ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-		self.ser.parity = serial.PARITY_NONE #set parity check: no parity
-		self.ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-		self.ser.xonxoff = False     #disable software flow control
-		self.ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-		self.ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-		self.noteOffset=60-36
-			
-		try: 
-			self.ser.open()
-		except Exception, e:
-			print "error open serial port: " + str(e)
-			exit()
-		self.ser.write(chr(255));
-
-	def setTone(self,nr, onOff):
-		if nr<0 or nr>n_octave*12:
-			print "-----------",nr,"out of range"
-			return
-		if onOff:
-			self.ser.write(chr(nr|(1<<7)))
-		else:
-			self.ser.write(chr(nr))
-	def playMidi(self,filename,includeTracks=[]):
-		mid = MidiFile(filename)
-		events=[]
-		print "Number of tracks:",len(mid.tracks)
-		tempo=600000
-		for i, track in enumerate(mid.tracks):
-			print('Track {}: {}'.format(i, track.name))
-			if not (includeTracks==[] or i in includeTracks):
-				continue
-			t=0
-			for message in track:
-				t+=message.time
-				if (message.type=="note_on" or message.type=="note_off") :
-					e={}
-					if message.velocity==0:
-						e["type"]="note_off"
-					else:
-						e["type"]=message.type
-					e["note"]=message.note
-					events.append((t,e))
-				elif message.type=="set_tempo":
-					e={}
-					e["type"]=message.type
-					e["tempo"]=message.tempo
-					events.append((t,e))
-		events.sort(key=lambda tup: tup[0])
-		last_time=0
-		t=0
-		for e in events:
-			t=e[0]
-			time.sleep(((t-last_time)*10**(-6)/480.*tempo))
-			last_time=t
-			ev=e[1]
-			if ev["type"]=="note_on":
-				print "PLAY  ",ev["note"]-self.noteOffset
-				self.setTone(ev["note"]-self.noteOffset,True);
-			elif ev["type"]=="note_off":
-				print "STOP ",ev["note"]-self.noteOffset
-				self.setTone(ev["note"]-self.noteOffset,False);
-			elif ev["type"]=="set_tempo":
-					tempo=ev["tempo"]
-					print "TEMPO ", tempo,"=",60000000./tempo,"BPM"
-					
-					
-
-
-synth=Synth()
-
-
-synth.noteOffset=60-36
-synth.playMidi("elise2.mid")
-
-#~ synth.setTone(0,True)
-#~ time.sleep(1)
-#~ synth.setTone(0,False)
-
-
+with open(hfilename, "w") as hfile:
+    with open(cfilename, "w") as cfile:
+        synth=Synth(60 - 36, hfile, cfile)
+        # for ... in os.walk("../music"):
+        synth.writeCArray("elise2.mid", "elise")
