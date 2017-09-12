2da394c28c9d2d07e091797937efe7e88f9efc3d
Robert Schütz <robert.schuetz@stud.uni-heidelberg.de>
2017-09-12 13:58:31 +0200

    Another try

--- slave/main.c
+++ slave/main.c
@@ -12,21 +12,31 @@
 
 void init();
 
+/*SIGNAL(TIMER1_COMPA_vect)
+{
+    PINB ^= (1 << 1);
+}*/
+
 int main()
 {
+    init();
+
+    OCR1A = 500;
+    while(1);
+
     uint8_t i = 0;
     while (1)
     {
         uint16_t delay = pgm_read_word(&elise[i]);
         if (i > 0 && delay == 0)
         {
-            PINB = 0;
+            OCR1A = 0;
             break;
         }
-        i += 2;
         reset_timer();
         while(getMsTimer() < delay);
         OCR1A = pgm_read_word(&elise[i+1]);
+        i += 2;
     }
     OCR1A = 0;
 }
@@ -39,9 +49,10 @@ void init()
     DDRB |= (1 << 1); // set B1 to output
 
     // Timer1
-    // Fast PWM, TOP is OCR1A
-    TCCR1A = (1 << WGM13) | (1 << WGM12) | (1 << WGM11) | (1 << WGM10);
-    TCCR1A |= (1 << COM1A1); // toggle pin OC1A (B1) on compare match
-    TCCR1B = (1 << CS11); // prescaler = 8
+    //TCCR1A = (1 << WGM12); // CTC, TOP is OCR1A
+    //TCCR1A |= (1 << COM1A0); // toggle pin OC1A (B1) on compare match
+    TCCR1A = (1 << WGM13) | (1 << WGM10);
+    TCCR1A |= (1 << COM1A1);
+    TCCR1B = (1 << CS12); // prescaler = 
     //TIMSK1 = (1 << OCIE1A); // enable interrupt
 }
